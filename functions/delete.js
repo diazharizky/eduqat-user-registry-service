9beff6e9b6e5de8f8b5e966abd918ca501d8f3a4
Diaz Harizky Firdaus <diazharizky@gmail.com>
2023-08-08 11:37:38 +0700

    Add delete functionality

---
+++ functions/delete.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const { DeleteCommand } = require('@aws-sdk/lib-dynamodb')
+const dbClient = require('../libs/dbClient');
+const { success, fatal } = require('../libs/responses')
+
+module.exports.handler = async (e) => {
+  try {
+    await dbClient.send(new DeleteCommand({
+      TableName: process.env.DB_TABLE,
+      Key: {
+        id: e.pathParameters.id
+      }
+    }))
+
+    return success({
+      message: "User successfully deleted"
+    })
+  } catch (err) {
+    console.log(err)
+
+    return fatal("Error unable to delete user")
+  }
+}
